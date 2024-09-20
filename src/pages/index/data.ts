34dead4641ae34b2e9cbb8804a72996102391e56
Tangjj1996 <awesometangji@gmail.com>
2024-09-20 20:37:53 +0800

    feat: 卡片列表字段

--- src/pages/index/data.ts
+++ src/pages/index/data.ts
@@ -2,49 +2,100 @@ export const data = [
   {
     key: "1",
     text: "字节前端面试 1，",
-    cover: "",
-    avatar: "",
+    cover:
+      "https://sns-webpic-qc.xhscdn.com/202409202021/de6d80d2533c6172bc0a1466b5a9737d/1040g2sg317deabnp44dg5n6d2np4mq060mg2u8o!nc_n_webp_mw_1",
+    avatar:
+      "https://sns-avatar-qc.xhscdn.com/avatar/5ccd15f20000000012036806.jpg?imageView2/2/w/60/format/webp|imageMogr2/strip",
     like_count: 100,
+    author: "顾建杰",
     isLiked: true,
   },
   {
     key: "2",
     text: "字节前端面试 1，",
-    cover: "",
-    avatar: "",
+    cover:
+      "https://sns-webpic-qc.xhscdn.com/202409202021/de6d80d2533c6172bc0a1466b5a9737d/1040g2sg317deabnp44dg5n6d2np4mq060mg2u8o!nc_n_webp_mw_1",
+    avatar:
+      "https://sns-avatar-qc.xhscdn.com/avatar/5ccd15f20000000012036806.jpg?imageView2/2/w/60/format/webp|imageMogr2/strip",
     like_count: 100,
+    author: "顾建杰",
     isLiked: true,
   },
   {
     key: "3",
     text: "字节前端面试 1，",
-    cover: "",
-    avatar: "",
+    cover:
+      "https://sns-webpic-qc.xhscdn.com/202409202021/de6d80d2533c6172bc0a1466b5a9737d/1040g2sg317deabnp44dg5n6d2np4mq060mg2u8o!nc_n_webp_mw_1",
+    avatar:
+      "https://sns-avatar-qc.xhscdn.com/avatar/5ccd15f20000000012036806.jpg?imageView2/2/w/60/format/webp|imageMogr2/strip",
     like_count: 100,
+    author: "顾建杰",
     isLiked: true,
   },
   {
     key: "4",
-    text: "字节前端面试 1，面试没有结果啊啊啊，字节前端面试 1，面试没有结果啊啊啊字节前端面试 1，面试没有结果啊啊啊字节前端面试 1，面试没有结果啊啊啊字节前端面试 1，面试没有结果啊啊啊",
-    cover: "",
-    avatar: "",
+    text: "字节前端面试 1，",
+    cover:
+      "https://sns-webpic-qc.xhscdn.com/202409202021/de6d80d2533c6172bc0a1466b5a9737d/1040g2sg317deabnp44dg5n6d2np4mq060mg2u8o!nc_n_webp_mw_1",
+    avatar:
+      "https://sns-avatar-qc.xhscdn.com/avatar/5ccd15f20000000012036806.jpg?imageView2/2/w/60/format/webp|imageMogr2/strip",
     like_count: 100,
+    author: "顾建杰",
     isLiked: true,
   },
   {
     key: "5",
-    text: "字节前端面试 1，",
-    cover: "",
-    avatar: "",
+    text: "字节前端面试 1哈哈哈哈哈附加法师法师发发撒粉啊发发发发发发法",
+    cover:
+      "https://sns-webpic-qc.xhscdn.com/202409202021/de6d80d2533c6172bc0a1466b5a9737d/1040g2sg317deabnp44dg5n6d2np4mq060mg2u8o!nc_n_webp_mw_1",
+    avatar:
+      "https://sns-avatar-qc.xhscdn.com/avatar/5ccd15f20000000012036806.jpg?imageView2/2/w/60/format/webp|imageMogr2/strip",
     like_count: 100,
-    isLiked: false,
+    author: "顾建杰",
+    isLiked: true,
   },
   {
     key: "6",
     text: "字节前端面试 1，",
-    cover: "",
-    avatar: "",
+    cover:
+      "https://sns-webpic-qc.xhscdn.com/202409202021/de6d80d2533c6172bc0a1466b5a9737d/1040g2sg317deabnp44dg5n6d2np4mq060mg2u8o!nc_n_webp_mw_1",
+    avatar:
+      "https://sns-avatar-qc.xhscdn.com/avatar/5ccd15f20000000012036806.jpg?imageView2/2/w/60/format/webp|imageMogr2/strip",
     like_count: 100,
-    isLiked: false,
+    author: "顾建杰",
+    isLiked: true,
+  },
+  {
+    key: "7",
+    text: "字节前端面试 1，",
+    cover:
+      "https://sns-webpic-qc.xhscdn.com/202409202021/de6d80d2533c6172bc0a1466b5a9737d/1040g2sg317deabnp44dg5n6d2np4mq060mg2u8o!nc_n_webp_mw_1",
+    avatar:
+      "https://sns-avatar-qc.xhscdn.com/avatar/5ccd15f20000000012036806.jpg?imageView2/2/w/60/format/webp|imageMogr2/strip",
+    like_count: 100,
+    author: "顾建杰",
+    isLiked: true,
+  },
+  {
+    key: "8",
+    text: "字节前端面试 1，",
+    cover:
+      "https://sns-webpic-qc.xhscdn.com/202409202021/de6d80d2533c6172bc0a1466b5a9737d/1040g2sg317deabnp44dg5n6d2np4mq060mg2u8o!nc_n_webp_mw_1",
+    avatar:
+      "https://sns-avatar-qc.xhscdn.com/avatar/5ccd15f20000000012036806.jpg?imageView2/2/w/60/format/webp|imageMogr2/strip",
+    like_count: 100,
+    author: "顾建杰",
+    isLiked: true,
+  },
+  {
+    key: "9",
+    text: "字节前端面试 1，",
+    cover:
+      "https://sns-webpic-qc.xhscdn.com/202409202021/de6d80d2533c6172bc0a1466b5a9737d/1040g2sg317deabnp44dg5n6d2np4mq060mg2u8o!nc_n_webp_mw_1",
+    avatar:
+      "https://sns-avatar-qc.xhscdn.com/avatar/5ccd15f20000000012036806.jpg?imageView2/2/w/60/format/webp|imageMogr2/strip",
+    like_count: 100,
+    author: "顾建杰",
+    isLiked: true,
   },
 ];
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,5 @@
 import { navigateTo } from "@tarojs/taro";
-import { View, Image } from "@tarojs/components";
+import { View, Image, ITouchEvent } from "@tarojs/components";
 import heart from "@/assets/icon/heart.svg";
 import heartFill from "@/assets/icon/heart-fill.svg";
 import { data } from "./data";
@@ -16,6 +16,10 @@ export default function Index() {
 
     if (index % 2 === 0) {
       objStyle.transform = `translateY(-100px)`;
+    }
+
+    if (index === data.length - 1 && index % 2 === 0) {
+      objStyle.transform += `translateX(-105px)`;
     }
 
     objStyle.height = 300;
@@ -36,38 +40,54 @@ export default function Index() {
     return objStyle;
   };
 
+  /**
+   * 跳转详情
+   * @param key
+   */
   const handleClick = (key: string) => {
     navigateTo({
       url: `/pages/detail/index?key=${key}`,
     });
   };
 
+  const handleLiked = (e: ITouchEvent, key: string) => {
+    e.stopPropagation();
+  };
+
   return (
     <View className={styles.index}>
-      {data.map(({ key, cover, text, avatar, isLiked, like_count }, index) => (
-        <View
-          key={key}
-          style={calcStyle(index)}
-          className={styles.item}
-          onClick={() => handleClick(key)}
-        >
-          <Image src={cover} style={clacImgStyle(index)} />
-          <View className={styles["item-text"]}>{text}</View>
-          <View className={styles["item-user"]}>
-            <Image
-              src={avatar}
-              style={{ width: 20, height: 20, borderRadius: "50%" }}
-            />
-            <View className={styles["item-user-like"]}>
-              <Image
-                src={isLiked ? heartFill : heart}
-                style={{ width: 20, height: 20, borderRadius: "50%" }}
-              />
-              {like_count}
+      {data.map(
+        ({ key, cover, text, avatar, author, isLiked, like_count }, index) => (
+          <View
+            key={key}
+            style={calcStyle(index)}
+            className={styles.item}
+            onClick={() => handleClick(key)}
+          >
+            <Image src={cover} style={clacImgStyle(index)} />
+            <View className={styles["item-text"]}>{text}</View>
+            <View className={styles["item-user"]}>
+              <View className={styles["item-user-name"]}>
+                <Image
+                  src={avatar}
+                  style={{ width: 20, height: 20, borderRadius: "50%" }}
+                />
+                <View>{author}</View>
+              </View>
+              <View
+                className={styles["item-user-like"]}
+                onClick={(e) => handleLiked(e, key)}
+              >
+                <Image
+                  src={isLiked ? heartFill : heart}
+                  style={{ width: 20, height: 20, borderRadius: "50%" }}
+                />
+                {like_count}
+              </View>
             </View>
           </View>
-        </View>
-      ))}
+        )
+      )}
     </View>
   );
 }
