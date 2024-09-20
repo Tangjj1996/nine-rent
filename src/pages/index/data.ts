93876e48407b36c0883b5eca2fb671a181610d3b
Tangjj1996 <awesometangji@gmail.com>
2024-09-20 17:55:16 +0800

    feat: 首页样式调整

--- src/pages/index/data.ts
+++ src/pages/index/data.ts
@@ -1,7 +1,42 @@
 export const data = [
   {
     key: "1",
-    text: "字节免死",
+    text: "字节前端面试 1，",
+    cover: "",
+    avatar: "",
+    like: 1,
+  },
+  {
+    key: "2",
+    text: "字节前端面试 1，",
+    cover: "",
+    avatar: "",
+    like: 1,
+  },
+  {
+    key: "3",
+    text: "字节前端面试 1，",
+    cover: "",
+    avatar: "",
+    like: 1,
+  },
+  {
+    key: "4",
+    text: "字节前端面试 1，面试没有结果啊啊啊，字节前端面试 1，面试没有结果啊啊啊字节前端面试 1，面试没有结果啊啊啊字节前端面试 1，面试没有结果啊啊啊字节前端面试 1，面试没有结果啊啊啊",
+    cover: "",
+    avatar: "",
+    like: 1,
+  },
+  {
+    key: "5",
+    text: "字节前端面试 1，",
+    cover: "",
+    avatar: "",
+    like: 1,
+  },
+  {
+    key: "6",
+    text: "字节前端面试 1，",
     cover: "",
     avatar: "",
     like: 1,
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,22 +1,49 @@
-import { View } from "@tarojs/components";
+import { View, Image } from "@tarojs/components";
 import { data } from "./data";
 import styles from "./styles.module.less";
 
-console.log(styles);
-
 export default function Index() {
+  const calcStyle = (index: number): React.CSSProperties => {
+    const objStyle: React.CSSProperties = {};
+
+    if (index === 0) {
+      objStyle.height = 200;
+      return objStyle;
+    }
+
+    if (index % 2 === 0) {
+      objStyle.transform = `translateY(-100px)`;
+    }
+
+    objStyle.height = 300;
+    return objStyle;
+  };
+
+  const clacImgStyle = (index: number): React.CSSProperties => {
+    const objStyle: React.CSSProperties = {
+      width: "100%",
+    };
+
+    if (index === 0) {
+      objStyle.height = 100;
+      return objStyle;
+    }
+
+    objStyle.height = 200;
+    return objStyle;
+  };
+
   return (
     <View className={styles.index}>
       {data.map(({ key, cover, text, avatar, like }, index) => (
-        <View
-          key={key}
-          style={{ height: index === 0 ? 200 : 300 }}
-          className={styles.item}
-        >
-          <View>{cover}</View>
-          <View>{text}</View>
-          <View>
-            <View>{avatar}</View>
+        <View key={key} style={calcStyle(index)} className={styles.item}>
+          <Image src={cover} style={clacImgStyle(index)} />
+          <View className={styles["item-text"]}>{text}</View>
+          <View className={styles["item-user"]}>
+            <Image
+              src={avatar}
+              style={{ width: 20, height: 20, borderRadius: "50%" }}
+            />
             <View>{like}</View>
           </View>
         </View>
