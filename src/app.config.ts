a961d637986399161875baff02d6fbd67d0ca073
Tangjj1996 <awesometangji@gmail.com>
2024-09-20 16:42:14 +0800

    feat: 配置 tabar 页面

--- src/app.config.ts
+++ src/app.config.ts
@@ -11,18 +11,25 @@ export default defineAppConfig({
     navigationBarTextStyle: 'black'
   },
   tabBar: {
+    selectedColor: '#3b82f6',
     list: [
       {
         pagePath: 'pages/index/index',
-        text: '首页'
+        text: '首页',
+        iconPath: './assets/icon/house.png',
+        selectedIconPath: './assets/icon/house_blue.png',
       },
       {
         pagePath: 'pages/search/index',
-        text: '搜索'
+        text: '搜索',
+        iconPath: './assets/icon/search.png',
+        selectedIconPath: './assets/icon/search_blue.png',
       },
       {
         pagePath: 'pages/mine/index',
-        text: '我的'
+        text: '我的',
+        iconPath: './assets/icon/user.png',
+        selectedIconPath: './assets/icon/user_blue.png',
       },
     ]
   }
