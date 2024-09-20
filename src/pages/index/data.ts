a60d0ea6e011ec550f96726c3659c89965c3ba76
Tangjj1996 <awesometangji@gmail.com>
2024-09-20 17:27:05 +0800

    feat: 单位转换

---
+++ src/pages/index/data.ts
@@ -0,0 +1,9 @@
+export const data = [
+  {
+    key: "1",
+    text: "字节免死",
+    cover: "",
+    avatar: "",
+    like: 1,
+  },
+];
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,26 @@
-/* eslint-disable jsx-quotes */
-import { View, Text } from "@tarojs/components";
-import { useLoad } from "@tarojs/taro";
-import "./index.less";
+import { View } from "@tarojs/components";
+import { data } from "./data";
+import styles from "./styles.module.less";
+
+console.log(styles);
 
 export default function Index() {
-  useLoad(() => {
-    console.log("Page loaded");
-  });
-
   return (
-    <View className="index">
-      <Text>Hello world</Text>
+    <View className={styles.index}>
+      {data.map(({ key, cover, text, avatar, like }, index) => (
+        <View
+          key={key}
+          style={{ height: index === 0 ? 200 : 300 }}
+          className={styles.item}
+        >
+          <View>{cover}</View>
+          <View>{text}</View>
+          <View>
+            <View>{avatar}</View>
+            <View>{like}</View>
+          </View>
+        </View>
+      ))}
     </View>
   );
 }
