53414008438442ea966ff75d926409c36341726f
Tangjj1996 <awesometangji@gmail.com>
2024-09-26 16:57:15 +0800

    feat: 状态值提升到store

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -7,7 +7,6 @@ import {
   login,
   getStorageSync,
   setStorageSync,
-  useDidShow,
 } from "@tarojs/taro";
 import { View, Image, ITouchEvent } from "@tarojs/components";
 import heart from "@/assets/icon/heart.svg";
@@ -17,12 +16,12 @@ import { postLike, postCancelLike } from "@/service/hourse/postLike";
 import { exceptionBiz } from "@/lib/utils";
 import { LocalStorageKey } from "@/enums";
 import { getLogin } from "@/service/user/login";
-import { ListData } from "@/service/hourse/List";
+import { useHomeStore } from "@/store/homeStore";
 import styles from "./styles.module.less";
 
 export default function Index() {
   const isLoaded = useRef(false);
-  const [data, setData] = useState<ListData>();
+  const homeStore = useHomeStore();
   const [pageInfo, setPageInfo] = useState({
     loading: false,
     isNextLoading: false,
@@ -36,19 +35,20 @@ export default function Index() {
       const {
         data: { data: listData },
       } = await getList({
-        current: data?.current! + 1,
-        page_size: data?.page_size!,
+        current: homeStore?.current! + 1,
+        page_size: homeStore?.page_size!,
       });
-      setData((state) => ({
+      useHomeStore.setState((state) => ({
         current: listData.current,
         page_size: listData.page_size,
         total: listData.total,
-        list: state?.list.concat(listData.list) ?? [],
+        list: state?.list?.concat(listData.list) ?? [],
       }));
       setPageInfo((state) => ({
         ...state,
         hasMore:
-          (data?.list.length ?? 0) + listData.list.length < listData.total,
+          (homeStore?.list?.length ?? 0) + listData.list.length <
+          listData.total,
       }));
     } catch (e) {
       exceptionBiz(e);
@@ -68,7 +68,7 @@ export default function Index() {
       const {
         data: { data: listData },
       } = (await getList({ current: 1, page_size: 10 })) || {};
-      setData(listData);
+      useHomeStore.setState(listData);
       setPageInfo((state) => ({
         ...state,
         hasMore: listData.list.length < listData.total,
@@ -85,28 +85,6 @@ export default function Index() {
     }
   });
 
-  // useDidShow(async () => {
-  //   if (!isLoaded.current) return;
-  //   try {
-  //     const { current, page_size, total } = data || {};
-  //     const {
-  //       data: { data: listData },
-  //     } = await getList({ current: current ?? 1, page_size: page_size ?? 10 });
-  //     setData(
-  //       produce(data, (draft) => {
-  //         if (draft?.list) {
-  //           const len = listData.list.length;
-  //           const index = (total ?? 0) - len;
-  //           draft.list = draft.list.slice(0, index);
-  //           draft.list = draft.list.concat(listData.list);
-  //         }
-  //       })
-  //     );
-  //   } catch (e) {
-  //     exceptionBiz(e);
-  //   }
-  // });
-
   const calcStyle = (index: number): React.CSSProperties => {
     const objStyle: React.CSSProperties = {};
 
@@ -120,7 +98,7 @@ export default function Index() {
       objStyle.transform = `translateY(-100px)`;
     }
 
-    if (index === data?.list.length! - 1 && index % 2 === 0) {
+    if (index === homeStore?.list?.length! - 1 && index % 2 === 0) {
       if (!objStyle.transform) {
         objStyle.transform = "";
       }
@@ -165,9 +143,9 @@ export default function Index() {
       const {
         data: { data: listData },
       } = await postLike({ id });
-      setData(
-        produce(data, (draft) => {
-          draft?.list.forEach((item) => {
+      useHomeStore.setState(
+        produce(homeStore, (draft) => {
+          draft?.list?.forEach((item) => {
             if (item.id === listData.id) {
               item.is_liked = true;
               item.like_count++;
@@ -191,9 +169,9 @@ export default function Index() {
       const {
         data: { data: listData },
       } = await postCancelLike({ id });
-      setData(
-        produce(data, (draft) => {
-          draft?.list.forEach((item) => {
+      useHomeStore.setState(
+        produce(homeStore, (draft) => {
+          draft?.list?.forEach((item) => {
             if (item.id === listData.id) {
               item.is_liked = false;
               item.like_count--;
@@ -208,7 +186,7 @@ export default function Index() {
 
   return (
     <View className={styles.index}>
-      {data?.list?.map(
+      {homeStore?.list?.map(
         (
           { key, id, cover, title, avatar, nick_name, is_liked, like_count },
           index
---
+++ src/store/collectionStore.ts
@@ -0,0 +1,3 @@
+import { create } from "zustand";
+
+export const useCollectionStore = create()((set) => {});
